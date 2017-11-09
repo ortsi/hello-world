e1a2c786246fcdffa23a9a8d5bae56190ca2e463
Or Gat <orga@jfrog.com>
2017-11-09 11:01:48 +0200

    Update sampbash.sh

--- sampbash.sh
+++ sampbash.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-    mvn -version
-    java -version
-    which mvn 
+#    mvn -version
+#    java -version
+#    which mvn 
+    echo START
     export M2_SAMP=$(realpath $(dirname $(readlink -f $(which mvn)))/../../)
     echo $M2_SAMP
-    
+    echo FINISH
 
