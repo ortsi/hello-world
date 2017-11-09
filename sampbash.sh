d102824b938d03d3bfd44a038be37680da134f19
Or Gat <orga@jfrog.com>
2017-11-09 11:07:50 +0200

    Update sampbash.sh

--- sampbash.sh
+++ sampbash.sh
@@ -6,7 +6,7 @@
     echo START
     #export M2_SAMP=$(realpath $(dirname $(readlink -f $(which mvn)))/../../)
     
-    export M2_SAMP=$(dirname $(readlink -f $(which mvn)))/../../
+    export M2_SAMP=$(readlink $(dirname $(readlink -f $(which mvn)))/../../)
     echo $M2_SAMP
     echo FINISH
 
